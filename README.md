134b53d138ff7a7f032f71854fbba21a0dd9450a
𝑻𝑰𝑻𝑼𝑳𝑨𝑹 𝑱𝑶𝑺𝑿 <benjaminmijahuanga@gmail.com>
2024-07-18 22:50:55 -1100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
-# Personalisaciones
+### 🔮 XIA BOT-MD 🔮
+<p align="center">
+<img src="https://telegra.ph/file/39b0aa3fe324550a3000a.jpg" alt="XiaBot-MD" width="900"/>
+</p>
+
+#### Version De Xia: 3.3.0 
+
+#### Creador y Dueño: Angelito⚡
+
+
+### CONTACTOS 💻
+<p>
+
+
+ <a href="http://www.instagram.com/usxr_angelito" target="blank"><img src="https://img.shields.io/badge/Instagram-30302f?style=flat&logo=instagram" /></a>
+<a href="https://api.whatsapp.com/send/?phone=59897246324&text=Hola 👋 soporte de Xia Bot &type=phone_number&app_absent=0" target="blank"><img src="https://img.shields.io/badge/Whatsapp-30302f?style=flat&logo=whatsapp" /></a>
+
+</p> 
+
+
+
+😃𝗦𝗜 𝗡𝗢 𝗥𝗘𝗦𝗣𝗢𝗡𝗗𝗘 𝗘𝗦 𝗣𝗢𝗥 𝗤𝗨𝗘 𝗘𝗟 𝗡𝗨𝗠𝗘𝗥𝗢 𝗘𝗦𝗧𝗔 𝗘𝗡 𝗦𝗢𝗣𝗢𝗥𝗧𝗘 𝗬 𝗘𝗡 𝗘𝗦𝗘 𝗖𝗔𝗦𝗢 𝗛𝗔𝗕𝗟𝗘 𝗖𝗢𝗡 𝗘𝗟 𝗦𝗧𝗔𝗙𝗙💖
+
+
+
+
+### 👨‍💻 INSTALACIÓN TERMUX
+> **Note** Comandos para instalar de forma manual en termux o otra app
+```bash
+termux-setup-storage
+```
+```bash
+apt update && apt upgrade && pkg install -y git nodejs ffmpeg imagemagick yarn
+```
+```bash
+git clone https://github.com/Karim-off/XiaBot-Pro && cd XiaBot-Pro
+```
+```bash
+yarn install
+```
+```bash
+npm install
+```
+```bash
+npm update
+```
+```bash
+npm start
+```
+> **Warning** Si aparece (Y/I/N/O/D/Z) [default=N] ? use la letra "y" + "ENTER" para continuar con la instalación 
+
+* #### APLICACIÓN RECOMENDADA PARA [`DESCOMPRIMIR`](https://play.google.com/store/apps/details?id=com.rarlab.rar)
+* #### APLICACIÓN RECOMENDADA PARA EDITAR [`NÚMERO DE OWNER`](https://play.google.com/store/apps/details?id=com.rhmsoft.code)
+> **Note** Guardar los archivos en la ubicación: storage/downloads/RubyBot-MD-master/RubyBot-MD-master   
+----
+### 💟 USAR XIA 24/7 EN TERMUX 
+> Ejecutar estos comandos dentro de la carpeta XiaBot-MD
+```bash
+termux-wake-lock && npm i -g pm2 && pm2 start index.js && pm2 save && pm2 logs
+```
+### ⚡ REPLIT - XIABOT
+<a target="_blank" href="https://replit.com/github/karim-off/XiaBot-Pro"><img alt="Run on Replit" src="https://binbashbanana.github.io/deploy-buttons/buttons/remade/replit.svg"></a>
+<a href="https://replit.com/github/GataNina-Li/GataBot-MD"> <img src="https://media0.giphy.com/media/lMwu8EJAnv9kmn51KQ/giphy.gif" height="29px"></a>
+------------------
+- [x] Resultado <details><summary>Importar Repositorio - XiaBot-Pro</summary><img src="https://i.imgur.com/GQyRnMf.jpg"></details>
+------------------
+
+### 💬 DUDAS DEL BOT 
+<a href="http://wa.me/59897246324" target="blank"><img src="https://img.shields.io/badge/KARIM_CREADOR-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+
+### 🤖 GRUPO DE SOPORTE
+<a href="https://chat.whatsapp.com/LcFTUnvu0Tw1tCnA2ybdR6" target="blank"><img src="https://img.shields.io/badge/GRUPO_DE_SOPORTE-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+
+### 📄 CANAL DE WHATSAPP
+<a href="https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y" target="blank"><img src="https://img.shields.io/badge/CANAL_DE_BOT-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />
+
+### USUARIOS AUTORIZADOS
+
+<details>
+<summary><b>¿Qué significa ser un Distribuidor Autorizado o reseller en GataBot?</b></summary>
+
+- Aquellos usuarios que adquieran la licencia de Distribuidores Autorizados o resellers obtienen el derecho legal de utilizar este repositorio para fines educativos, políticos o comerciales, sin poner en riesgo su reputación personal u organizacional.
+</details>
+
+<details>
+<summary><b>¿Dónde puedes solicitar la licencia para ser Distribuidor Autorizado o reseller de GataBot?</b></summary>
+
+  - Te ofrecemos opciones exclusivas: **[Instagram](https://www.instagram.com/gata_dios/)**, **[Telegram](https://t.me/SoporteGataBot)**, y **[correo electrónico](centergatabot@gmail.com)**.
+</details>
+
+
+### 🔮 CREADOR 
+[![Karim-off](https://github.com/Karim-off.png?size=100)](https://github.com/Karim-off) 
+> Copyright (c) 2024 **[Karim-off](https://github.com/Karim-off/XiaBot-Pro/blob/master/LICENSE)**.
---
+++ config.js
@@ -0,0 +1,109 @@
+import {watchFile, unwatchFile} from 'fs';
+import chalk from 'chalk';
+import {fileURLToPath} from 'url';
+import fs from 'fs'; 
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
+
+global.botnumber = ""
+
+global.owner = [
+  ['51936732723', '𝙈𝙄𝙆𝘼𝙎𝘼 𝘽𝙊𝙏', true],
+  ['51936732723', '𝘾𝙍𝙀𝘼𝘿𝙊𝙍', true],
+  ['51936732723', '𝙎𝙊𝙋𝙊𝙍𝙏𝙀', true],
+  ['', '', true],
+  ['', '', true],
+  ['', '', true],
+  ['', '', true],
+  ['', '', false],
+  ['', '', true],
+  ['', '', true],
+  [''],
+  [''],
+  [''],
+  [''],
+  [''],
+  [''],
+  [''], 
+  [''],  
+  [''], 
+  ['']
+];
+
+global.suittag = ['51936732723'];
+global.prems = ['51936732723'];
+
+global.packname = '𝙈𝙄𝙆𝘼𝙎𝘼 𝘽𝙊𝙏';
+global.author = '𝙈𝙄𝙆𝘼𝙎𝘼 𝘽𝙊𝙏';
+global.wm = '𝙈𝙄𝙆𝘼𝙎𝘼 𝘽𝙊𝙏';
+global.titulowm = '𝙈𝙄𝙆𝘼𝙎𝘼 𝘽𝙊𝙏';
+global.titulowm2 = `𝙈𝙄𝙆𝘼𝙎𝘼 𝘽𝙊𝙏`
+global.igfg = '𝙈𝙄𝙆𝘼𝙎𝘼 𝘽𝙊𝙏';
+global.wait = '𝙐𝙣 𝙢𝙤𝙢𝙚𝙣𝙩𝙤...';
+
+global.imagen1 = fs.readFileSync('./Menu2.jpg');
+global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
+global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
+global.imagen4 = fs.readFileSync('./Menu.png');
+global.imagen5 = fs.readFileSync('./src/+18-hot.jpg');
+global.imagen6 = fs.readFileSync('./Menu3.png');
+
+global.mods = [];
+
+//* *******Tiempo***************
+global.d = new Date(new Date + 3600000);
+global.locale = 'es';
+global.dia = d.toLocaleDateString(locale, {weekday: 'long'});
+global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'});
+global.mes = d.toLocaleDateString('es', {month: 'long'});
+global.año = d.toLocaleDateString('es', {year: 'numeric'});
+global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
+//* ****************************
+global.wm2 = `▸ ${dia} ${fecha}\n▸ 𝙈𝙄𝙆𝘼𝙎𝘼 𝘽𝙊𝙏`;
+global.gt = '𝙈𝙄𝙆𝘼𝙎𝘼 𝘽𝙊𝙏';
+global.mysticbot = '𝙈𝙄𝙆𝘼𝙎𝘼 𝘽𝙊𝙏';
+global.md = 'https://chat.whatsapp.com/C0WHVT1wectIMBxcf3h96q';
+global.mysticbot = 'https://chat.whatsapp.com/C0WHVT1wectIMBxcf3h96q';
+global.canalbot = 'https://whatsapp.com/channel/0029VajUEsCB4hdNTg04zh1u';
+global.ig = 'https://www.instagram.com/boybenjx7';
+global.github = 'https://github.com';
+global.gtb = 'https://github.com';
+global.waitt = '𝙐𝙣 𝙢𝙤𝙢𝙚𝙣𝙩𝙤...';
+global.waittt = '𝙐𝙣 𝙢𝙤𝙢𝙚𝙣𝙩𝙤...';
+global.waitttt = '𝙐𝙣 𝙢𝙤𝙢𝙚𝙣𝙩𝙤...';
+global.nomorown = '51936732723';
+global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
+global.cmenut = '❖––––––『';
+global.cmenub = '┊✦ ';
+global.cmenuf = '╰━═┅═━––––––๑\n';
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     ';
+global.dmenut = '*❖─┅──┅〈*';
+global.dmenub = '*┊»*';
+global.dmenub2 = '*┊*';
+global.dmenuf = '*╰┅────────┅✦*';
+global.htjava = '⫹⫺';
+global.htki = '*⭑•̩̩͙⊱•••• ☪*';
+global.htka = '*☪ ••••̩̩͙⊰•⭑*';
+global.comienzo = '• • ◕◕════';
+global.fin = '════◕◕ • •';
+global.botdate = `⫹⫺ Date :  ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}`; // Asia/Jakarta
+global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${moment.tz('America/Los_Angeles').format('HH:mm:ss')}`;// America/Los_Angeles
+global.fgif = {key: {participant: '0@s.whatsapp.net'}, message: {'videoMessage': {'title': wm, 'h': `Hmm`, 'seconds': '999999999', 'gifPlayback': 'true', 'caption': bottime, 'jpegThumbnail': fs.readFileSync('./Menu.png')}}};
+global.multiplier = 99;
+global.flaaa = [
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+  'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=',
+];
+//* ************************
+
+const file = fileURLToPath(import.meta.url);
+watchFile(file, () => {
+  unwatchFile(file);
+  console.log(chalk.redBright('Update \'config.js\''));
+  import(`${file}?update=${Date.now()}`);
+});
